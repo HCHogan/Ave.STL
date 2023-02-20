ac9325422c0307b0d442ef139d7c1806d85a4805
Hank <78535316+HCHogan@users.noreply.github.com>
2023-02-20 13:40:21 +0800

    Update iterator_category.hpp
    Category Fixed

--- iterator_category.hpp
+++ iterator_category.hpp
@@ -4,7 +4,7 @@
 namespace Ave {
 	struct input_iterator_tag {};
 	struct output_iterator_tag {};
-	struct forward_iterator_tag : public input_iterator_tag {};
+	struct forward_iterator_tag : public input_iterator_tag, public output_iterator_tag {};
 	struct bidirectional_iterator_tag : public forward_iterator_tag {};
 	struct random_access_iterator_tag : public bidirectional_iterator_tag {};
 };
